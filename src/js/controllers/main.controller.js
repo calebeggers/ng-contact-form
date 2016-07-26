0d9f150fbfef94d26f1e191aaf1f3f0e2bac69a0
calebeggers <calebeggers11@gmail.com>
2016-07-26 12:35:59 -0400

    Made contact entries display below form

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -1,52 +1,80 @@
 function MainController ($scope, SERVER, $http) {
 	$scope.buttonStatus = "button-alert"
+
+	$scope.nameFieldStatus = "form-field"
+	$scope.emailFieldStatus = "form-field"
+	$scope.websiteFieldStatus = "form-field"
+	$scope.messageFieldStatus = "form-field"
+
+	init();
+
+	function init () {
+		$http.get(SERVER.URL).then( (res) => {
+			$scope.contacts = res.data;
+		});
+	}
+
 	$scope.nameCheck = function () {
 		if ($scope.form.name || $scope.form.name === undefined) {
 			$scope.nameNotice = ""
-			$scope.buttonStatus = "button"
+			$scope.nameFieldStatus = "field-valid"
 		} else {
 			$scope.nameNotice = "Name cannot be left empty"
 			$scope.buttonStatus = "button-alert"
+			$scope.nameFieldStatus = "field-invalid"
 		}
 	}
 
 	$scope.emailCheck = function () {
 		if ($scope.form.email.includes("@")) {
 			$scope.emailNotice = ""
-			$scope.buttonStatus = "button"
+			$scope.emailFieldStatus = "field-valid"
 		} else {
 			$scope.emailNotice = "Email must contain '@'"
 			$scope.buttonStatus = "button-alert"
+			$scope.emailFieldStatus = "field-invalid"
 		}
 	}
 
 	$scope.websiteCheck = function () {
 		if ($scope.form.website.includes("http://")) {
 			$scope.websiteNotice = ""
-			$scope.buttonStatus = "button"
+			$scope.websiteFieldStatus = "field-valid"
 		} else if ($scope.form.website.includes("https://")) {
 			$scope.websiteNotice = ""
-			$scope.buttonStatus = "button"
+			$scope.websiteFieldStatus = "field-valid"
 		} else {
 			$scope.websiteNotice = "Website must start with 'http://' or 'https://'"
-			$scope.buttonStatus = "button-alert"
+			$scope.websiteFieldStatus = "field-invalid"
 		}
 	}
 
 	$scope.messageCheck = function () {
 		if ($scope.form.message || $scope.form.message === undefined) {
 			$scope.messageNotice = ""
-			$scope.buttonStatus = "button"
+			$scope.messageFieldStatus = "field-valid"
 		} else {
 			$scope.messageNotice = "Message cannot be left empty"
 			$scope.buttonStatus = "button-alert"
+			$scope.messageFieldStatus = "field-invalid"
+		}
+	}
+
+	$scope.buttonCheck = function () {
+		if ($scope.messageNotice !== "" ||
+			$scope.websiteNotice !== "" ||
+			$scope.emailNotice !== "" ||
+			$scope.nameNotice !== "") {
+			$scope.buttonStatus = "button-alert"
+		} else {
+			$scope.buttonStatus = "button"
 		}
 	}
 
 	$scope.submit = function (form) {
 		if ($scope.messageNotice !== "" ||
 			$scope.websiteNotice !== "" ||
-			$scope.emailNotice !== "" ||
+			$scope.emailNotice !== ""   ||
 			$scope.nameNotice !== "") {
 			console.log("Not submitted")
 		} else {
